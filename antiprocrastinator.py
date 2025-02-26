69c79bfdf1e5fe1ca45ad28e4dbf0338bdf63fe2
jstanle0 <jstanle0@byu.edu>
2025-02-26 10:03:08 -0700

    recieve input files

--- antiprocrastinator.py
+++ antiprocrastinator.py
@@ -5,8 +5,17 @@ def printHelp():
     print("python antiprocrastinator.py [availability-file] [tasks-file]")
 
 if __name__ == "__main__":
-    if len(sys.argv) > 3:
-        print("hi")
+    if len(sys.argv) == 3:
+        try:
+            with open(sys.argv[1], 'r') as availabilityFile:
+                availabilty = availabilityFile.readlines()
+            with open(sys.argv[2], 'r') as tasksFile:
+                tasks = tasksFile.readlines()
+        except:
+            print("Error: Invalid filenames")
+            sys.exit(1)
+        print (availabilty)
     else:
         printHelp()
+        sys.exit(1)
     
